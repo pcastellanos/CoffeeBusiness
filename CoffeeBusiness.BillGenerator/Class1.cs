b6bdb39b7ea3ec7e4d21053116df11a8b330bef1
Paula Alejandra Castellanos Medina <v-pcastellanos@youngliving.com>
2021-01-02 13:17:02 -0500

    init project

---
+++ CoffeeBusiness.BillGenerator/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CoffeeBusiness.BillGenerator
+{
+    public class Class1
+    {
+    }
+}
---
+++ CoffeeBusiness.BillPrinter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeBusiness.BillPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
